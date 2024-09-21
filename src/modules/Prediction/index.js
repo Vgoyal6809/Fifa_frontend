eedbeb64298f10f3bfc4f0aa4daaa7cc85ce8626
Vgoyal6809 <vgoyal6809@conestogac.on.ca>
2024-09-21 16:45:31 -0400

    Feat : Prediction Page UI

---
+++ src/modules/Prediction/index.js
@@ -0,0 +1,216 @@
+import React, { useEffect, useState } from 'react';
+import { CentreBachApi, GoalKeeperApi, LeftBackApi, LeftForwardApi, LeftMidApi, RightBackApi, RightForwardApi, RightMidApi } from '../../api';
+import { Avatar, AvatarContainer, Details, FieldOuterContainer, GoalBox, InstructionBox, InstructionPlayer, Player, PlayerDetails, PositionHeader, SemiCircle, SoccerField } from './styled';
+import Playerimg from './Player.png'
+
+
+
+// App Component
+const App = () => {
+  const [topPlayersData, setTopPlayersData] = useState('');
+  const [isFetching, setIsFetching] = useState(false);
+  const [isFailed, setIsFailed] = useState(false);
+  const [tilt, setTilt] = useState(false);
+  const [selectedPosition, setSelectedPosition] = useState('');
+
+  const [showAvatars, setShowAvatars] = useState(false);
+  const [selectedPlayer, setSelectedPlayer] = useState(null);
+  const [playersPositions, setPlayersPositions] = useState([]);
+  const [tutorial, setTutorial] = useState(true);
+
+  const Loaderplayers = [
+    { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 },
+    { id: 6 }, { id: 7 }, { id: 8 }, { id: 9 }, { id: 10 },
+    { id: 11 }, { id: 12 }, { id: 13 }, { id: 14 }, { id: 15 },
+    { id: 16 }, { id: 17 }, { id: 18 }
+  ];
+
+  const getRandomPosition = () => {
+    const x = Math.random() * 100; // percentage
+    const y = Math.random() * 100; // percentage
+    return { left: `${x}%`, top: `${y}%` };
+  };
+
+  const updatePlayerPositions = () => {
+    const newPositions = Loaderplayers.map(() => getRandomPosition());
+    setPlayersPositions(newPositions);
+  };
+
+  useEffect(() => {
+    if (isFetching) {
+      const interval = setInterval(updatePlayerPositions, 500); // Update every 500ms
+      return () => clearInterval(interval);
+    }
+  }, [isFetching]);
+  const players = [
+    { id: 1, left: '9%', top: '50%', action: () => handlePlayerClick({ id: 1, position: 'Goal Keeper' }) }, // Goalkeeper
+    { id: 2, left: '19%', top: '15%', action: () => handlePlayerClick({ id: 2, position: 'Left Back' }) },
+    { id: 3, left: '19%', top: '40%', action: () => handlePlayerClick({ id: 3, position: 'Center Back' }) },
+    { id: 4, left: '19%', top: '65%', action: () => handlePlayerClick({ id: 4, position: 'Center Back' }) },
+    { id: 5, left: '19%', top: '90%', action: () => handlePlayerClick({ id: 5, position: 'Right Back' }) },
+    { id: 6, left: '28%', top: '28%', action: () => handlePlayerClick({ id: 6, position: 'Left Mid' }) },
+    { id: 7, left: '28%', top: '52%', action: () => handlePlayerClick({ id: 7, position: 'Unknown' }) },
+    { id: 8, left: '28%', top: '78%', action: () => handlePlayerClick({ id: 8, position: 'Right Mid' }) },
+    { id: 9, left: '38%', top: '52%', action: () => handlePlayerClick({ id: 9, position: 'Striker' }) },
+    { id: 10, left: '36%', top: '15%', action: () => handlePlayerClick({ id: 10, position: 'Left Forward' }) },
+    { id: 11, left: '36%', top: '88%', action: () => handlePlayerClick({ id: 11, position: 'Right Forward' }) },
+  ];
+
+
+  const handlePlayerClick = ({ id, position }) => {
+    if (tilt) {
+      setTilt(false);
+    }
+    setTutorial(false);
+    setSelectedPosition(position);
+
+    switch (id) {
+      case 1:
+        GoalKeeperApi({ setIsFetching, setTopPlayersData, setIsFailed })
+        break;
+
+      case 2:
+        LeftBackApi({ setIsFetching, setTopPlayersData, setIsFailed })
+        break;
+
+      case 3:
+        CentreBachApi({ setIsFetching, setTopPlayersData, setIsFailed })
+        break;
+
+      case 4:
+        CentreBachApi({ setIsFetching, setTopPlayersData, setIsFailed })
+        break;
+
+      case 5:
+        RightBackApi({ setIsFetching, setTopPlayersData, setIsFailed })
+        break;
+
+      case 6:
+        LeftMidApi({ setIsFetching, setTopPlayersData, setIsFailed })
+        break;
+
+      case 7:
+        console.log(`Player ${id} has an Unknown position`);
+        // Add logic for Unknown position
+        break;
+
+      case 8:
+        RightMidApi({ setIsFetching, setTopPlayersData, setIsFailed })
+        break;
+
+      case 9:
+        console.log(`Player ${id} is a Striker`);
+        // Add logic for Striker
+        break;
+
+      case 10:
+        LeftForwardApi({ setIsFetching, setTopPlayersData, setIsFailed })
+        break;
+
+      case 11:
+        RightForwardApi({ setIsFetching, setTopPlayersData, setIsFailed })
+        break;
+
+      default:
+        console.log(`Player ${id} has an unknown position: ${position}`);
+        break;
+    }
+
+  };
+
+  const handleAvatarClick = (avatar) => {
+    setSelectedPlayer(avatar);
+  };
+
+  useEffect(() => {
+    if (!isFetching && topPlayersData) { setTilt(true); setShowAvatars(true); }
+    console.log(isFetching)
+  }, [isFetching]);
+
+  return (
+    <FieldOuterContainer>
+      <h1>Forecasting the Best Players for Every Position</h1>
+
+      <SoccerField tilt={tilt}>
+        <GoalBox isPenalty={true} isLeft={true} />
+        <GoalBox isPenalty={true} isLeft={false} />
+        <GoalBox isPenalty={false} isLeft={true} />
+        <GoalBox isPenalty={false} isLeft={false} />
+
+        <SemiCircle isLeft={true} />
+        <SemiCircle isLeft={false} />
+
+        {isFetching ? <>
+          {Loaderplayers.map((player, index) => (
+            <Player key={player.id} style={playersPositions[index] || { left: '50%', top: '50%' }} loader={player.id % 2 == 0} />
+          ))}
+        </> :
+          <>
+            {players.map(player => (
+              <Player key={player.id} style={{ left: player.left, top: player.top }} onClick={player?.action} tutorial={player.id == 1 && tutorial} />
+            ))}
+          </>}
+
+
+        {/* Display avatars on both sides */}
+        {showAvatars && (
+          <AvatarContainer tilt={tilt}>
+            <div className='left' >
+              {topPlayersData?.data?.slice(0, 5).map(avatar => (
+                <Avatar src={Playerimg} key={avatar?.ID} onClick={() => handleAvatarClick(avatar)} />
+              ))}
+            </div>
+            <div className='right' >
+              {topPlayersData?.data.slice(5).map(avatar => (
+                <Avatar src={Playerimg} key={avatar?.ID} onClick={() => handleAvatarClick(avatar)} />
+              ))}
+            </div>
+          </AvatarContainer>
+        )}
+
+        {tutorial &&
+          <InstructionBox>
+            Click on the <br />
+            <InstructionPlayer><p>= </p><p>Players</p></InstructionPlayer>
+            to get Information
+          </InstructionBox>
+        }
+
+        {tilt && selectedPlayer && (
+          <PlayerDetails>
+            <img src={Playerimg} />
+            <Details>
+              <h3>{selectedPlayer?.Name}</h3>
+              <p>Age - 24  &nbsp;&nbsp;&nbsp;&nbsp; Height - 5'10"</p>
+              <table>
+                <tr>
+                  <th className='title'></th>
+                  <th className='title'>Actual</th>
+                  <th className='title'>Predicted</th>
+                </tr>
+                <tr>
+                  <td className='title'>Potential</td>
+                  <td className='value'>89.08</td>
+                  <td className='value'>92.91</td>
+                </tr>
+                <tr>
+                  <td className='title'>Wage</td>
+                  <td className='value'>$1.1M</td>
+                  <td className='value'>$1.25M</td>
+                </tr>
+              </table>
+            </Details>
+          </PlayerDetails>
+        )}
+
+        {tilt && (
+          <PositionHeader>
+            <h1>{selectedPosition}</h1>
+          </PositionHeader>
+        )}
+      </SoccerField>
+    </FieldOuterContainer>
+  );
+};
+
+export default App;
---
+++ src/modules/Prediction/styled.js
@@ -0,0 +1,318 @@
+import styled, { css, keyframes } from "styled-components";
+
+// Soccer field styled component with 3D effect and realistic markings
+export const FieldOuterContainer = styled.div`
+  padding-top: 50px;
+  height:90vh;
+  h1 {
+    text-align: center;
+    padding: 0;
+    margin: 0;
+    margin-top:10px;
+    font-size:36px;
+    font-family: 'Arial, sans-serif';
+  }
+  
+`;
+
+export const SoccerFieldAnimation = keyframes`
+  0% { 
+  transform: perspective(1000px) rotateX(45deg);
+  }
+  100% { 
+    transform: rotateX(0deg);
+  }
+`;
+
+
+export const SoccerField = styled.div`
+  width: 800px;
+  height: 500px;
+  background-color: green;
+  position: relative;
+  margin: 0px auto;
+  margin-top: 40px;
+  border: 5px solid white;
+  box-shadow: 0 20px 50px rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
+  background: repeating-linear-gradient(
+    to right,
+    #228b22,
+    #228b22 50px,
+    #006400 50px,
+    #006400 100px
+  );
+  transform: perspective(1000px) rotateX(45deg);
+  transform-origin: center;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 50%;
+    width: 2px;
+    height: 100%;
+    background-color: white;
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    border: 2px solid white;
+    transform: translate(-50%, -50%);
+  }
+
+  ${props => props.tilt && css`
+     animation: ${SoccerFieldAnimation} 2s ease-in-out forwards;
+  `}
+`;
+
+export const GoalBox = styled.div`
+  position: absolute;
+  width: ${props => (props.isPenalty ? '100px' : '50px')};
+  height: ${props => (props.isPenalty ? '200px' : '100px')};
+  border: 2px solid white;
+  top: 50%;
+  transform: translateY(-50%);
+  left: ${props => (props.isLeft ? '0' : 'auto')};
+  right: ${props => (props.isLeft ? 'auto' : '0')};
+`;
+
+export const SemiCircle = styled.div`
+  position: absolute;
+  width: 30px;
+  height: 60px;
+  border: 2px solid white;
+  border-bottom-right-radius: 100px;
+  border-top-right-radius: 100px;
+  top: 50%;
+
+  ${props => props.isLeft && css`
+    transform: translate(306%,-45%);
+  `};
+
+  ${props => !props.isLeft && css`
+    transform: rotate(180deg) translate(306%,45%);
+    right: 0;
+  `};
+`;
+
+
+/************************************* */
+const jump = keyframes`
+  0%, 100% {
+    transform: translateY(-30px);
+  }
+  50% {
+    transform: translateY(0px); /* Adjust this value for jump height */
+  }
+`;
+
+// Define keyframes for the glowing aura effect
+const glow = keyframes`
+  0% {
+    box-shadow: 0 0 10px rgba(255, 255, 0, 0.6); /* Initial light yellow aura */
+  }
+  50% {
+    box-shadow: 0 0 20px 10px rgba(255, 255, 0, 1); /* Brighter and thicker yellow border */
+  }
+  100% {
+    box-shadow: 0 0 10px rgba(255, 255, 0, 0); /* No glow */
+  }
+`;
+
+
+
+const Playerdesign =css`
+width: 20px;
+  height: 20px;
+  background-color: blue;
+  border-radius: 50%;
+`
+
+export const Player = styled.div`
+  ${Playerdesign}
+  position: absolute;
+  transform: translate(-50%, -50%);
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.5);
+  cursor: pointer;
+
+  ${props => props.loader && css`
+      background-color: red;
+    `}
+
+  ${props => props.tutorial && css`
+    animation: ${jump} 1.5s infinite, ${glow} 1.5s infinite;   `
+  }  
+`;
+
+export const Avatar = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: ${props => (props.isHighlighted ? '#00f' : '#f0f0f0')};
+  border: 2px solid ${props => (props.isHighlighted ? '#000' : '#ddd')};
+  margin: 10px;
+  cursor: pointer;
+  marginLeft: -100px;
+`;
+
+// Player Details Container (for display on the right side)
+export const PlayerDetails = styled.div`
+  position: absolute;
+  right: 11%;
+  bottom:0;
+  padding: 20px;
+  max-width: 300px;
+  height:75%;
+  display:flex;
+  flex-direction:column;
+  justify-content:space-around;  
+  align-items:center;
+
+  img{
+  width:180px;
+  }
+`;
+
+export const InstructionBox = styled.div`
+  position: absolute;
+  right:0;
+  bottom:0;
+  padding: 20px;
+  width: 40%;
+  color:yellow;
+  font-size:24px;
+  line-height:2;
+`;
+
+export const InstructionPlayer = styled.div`
+  ${Playerdesign}
+  display:flex;  
+  align-items:center;
+
+  p{
+    padding-left:5px;
+    
+    &:nth-child(1){
+      padding-left:25px;
+    }
+  }
+
+`
+export const Details = styled.div`
+
+  padding: 5px;
+  border-radius: 10px;
+  max-width: 300px;
+  color:white;
+  text-align:center;
+
+  p{
+  margin:0;
+  line-height:1.5;
+  }
+
+  table {
+    // border-collapse: collapse;
+    margin: 10px auto 5px; /* Center the table */
+    font-family: Arial, sans-serif;
+}
+
+td {
+    padding: 8px;
+    text-align: center; /* Center align text */
+    border: none; /* Remove borders */
+}
+
+th {
+    padding: 8px;
+}
+
+.title{
+    background-color: #000080; /* Dark blue for header */
+}
+
+.value{
+    background-color: #8DC26F; /* Light blue for other cells */
+
+}
+`;
+
+export const PositionHeader = styled.div `
+  position: absolute;
+  margin: 5px 0 0 0;
+  width:50%;
+  right:0;
+  display:flex;
+  justify-content:center; 
+
+  h1{
+   margin:0;
+   color:white;
+   width:max-content;
+   font-weight:600;
+   font-size:22px;
+   }
+
+`;
+
+export const AvatarContainer = styled.div`
+  position: absolute;
+  display: flex;
+  justify-Content: space-between;
+  marginTop: 20px;
+  width:100%;
+  height:100%;
+  z-index:-1;
+
+.left,.right{
+
+  display: flex;
+  flex-direction:column;
+  justify-content:space-around;
+  overflow:hidden;
+  
+}
+
+${props => props.tilt ? css`
+  .left{
+   animation: ${moveRightToLeft} 1.5s linear forwards;
+  }
+  .right{
+   animation: ${moveLeftToRight} 1.5s linear forwards;
+  }
+  `
+:
+css`
+opacity:0;
+`
+}
+`;
+
+export const moveLeftToRight = keyframes`
+  0% {
+    transform: translateX(20%);
+    opacity:0;
+  }
+  100% {
+    transform: translateX(100%);
+    opacity:1;
+  }
+`;
+
+export const moveRightToLeft = keyframes`
+  0% {
+    transform: translateX(20%);
+    opacity:0;
+  }
+  100% {
+    transform: translateX(-100%);
+    opacity:1;
+  }
+`;
